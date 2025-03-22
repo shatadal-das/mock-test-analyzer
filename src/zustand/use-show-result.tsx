3d84750c0a5c56c741e30d923fdc0425cfe4b3e7
sumit-dey-69 <sumitdey.dev@gmail.com>
2025-03-22 23:24:46 +0530

    feat: add persistence to Zustand stores using session storage

--- src/zustand/use-show-result.tsx
+++ src/zustand/use-show-result.tsx
@@ -1,4 +1,6 @@
-import { create } from 'zustand';
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import createSessionStorage from "../utils/session-storage";
 
 interface ShowResultState {
   showResult: boolean;
@@ -6,10 +8,19 @@ interface ShowResultState {
   setShowResult: (show: boolean) => void;
 }
 
-const useShowResult = create<ShowResultState>((set) => ({
-  showResult: false,
-  toggleShowResult: () => set((state) => ({ showResult: !state.showResult })),
-  setShowResult: (show) => set({ showResult: show }),
-}));
+const useShowResult = create<ShowResultState>()(
+  persist(
+    (set) => ({
+      showResult: false,
+      toggleShowResult: () =>
+        set((state) => ({ showResult: !state.showResult })),
+      setShowResult: (show) => set({ showResult: show }),
+    }),
+    {
+      name: "showResult",
+      storage: createSessionStorage,
+    },
+  ),
+);
 
 export default useShowResult;
--- src/zustand/use-test-code.tsx
+++ src/zustand/use-test-code.tsx
@@ -1,15 +1,25 @@
-import { create } from 'zustand'
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import createSessionStorage from "../utils/session-storage";
 
 interface TestCodeState {
-  testCode: string
-  setTestCode: (code: string) => void
-  resetTestCode: () => void
+  testCode: string;
+  setTestCode: (code: string) => void;
+  resetTestCode: () => void;
 }
 
-const useTestCode = create<TestCodeState>((set) => ({
-  testCode: '',
-  setTestCode: (code) => set({ testCode: code }),
-  resetTestCode: () => set({ testCode: '' })
-}))
+const useTestCode = create<TestCodeState>()(
+  persist(
+    (set) => ({
+      testCode: "",
+      setTestCode: (code: string) => set({ testCode: code }),
+      resetTestCode: () => set({ testCode: "" }),
+    }),
+    {
+      name: "testCode",
+      storage: createSessionStorage,
+    },
+  ),
+);
 
-export default useTestCode
+export default useTestCode;
