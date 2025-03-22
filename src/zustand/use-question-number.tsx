823090e5e14145257654e4fd3dd1699be71a7dd2
Shatadal Das <shatadaldas.dev@gmail.com>
2025-03-22 22:15:15 +0530

    refactor: improve formatting and logic in useQuestionNumber Zustand store

--- src/zustand/use-question-number.tsx
+++ src/zustand/use-question-number.tsx
@@ -1,4 +1,4 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 interface QuestionNumberState {
   currentQuestionNumber: number;
@@ -9,9 +9,22 @@ interface QuestionNumberState {
 
 const useQuestionNumber = create<QuestionNumberState>((set) => ({
   currentQuestionNumber: 1,
-  setQuestionNumber: (questionNumber: number) => set({ currentQuestionNumber: questionNumber }),
-  nextQuestion: () => set((state) => ({ currentQuestionNumber: (state.currentQuestionNumber + 1 < 0 ? 75 : state.currentQuestionNumber + 1) })),
-  previousQuestion: () => set((state) => ({ currentQuestionNumber: (state.currentQuestionNumber - 1 >= 75 ? 0 : state.currentQuestionNumber - 1) })),
+  setQuestionNumber: (questionNumber: number) =>
+    set({ currentQuestionNumber: questionNumber }),
+  nextQuestion: () =>
+    set((state) => ({
+      currentQuestionNumber:
+        state.currentQuestionNumber + 1 > 75
+          ? 1
+          : state.currentQuestionNumber + 1,
+    })),
+  previousQuestion: () =>
+    set((state) => ({
+      currentQuestionNumber:
+        state.currentQuestionNumber - 1 < 1
+          ? 75
+          : state.currentQuestionNumber - 1,
+    })),
 }));
 
 export default useQuestionNumber;
